9b31a1c60b39a6e2d495746891aa398e2537b3a3
pengczha <116861160+pengczha@users.noreply.github.com>
2024-06-20 16:33:34 +0800

    Create network.tf

---
+++ samples/testing/src/network.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "Databricks-RG"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "Spoke" {
+  name                = "Spoke-VNet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "Hub" {
+  name                = "Hub-VNet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "dbrpub" {
+  name                 = "pub-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.Spoke.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet" "dbrpri" {
+  name                 = "pri-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.Spoke.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
