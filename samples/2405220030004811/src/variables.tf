b9b0c29de383c9976571bbba723c376a7b19de7c
pengczha <116861160+pengczha@users.noreply.github.com>
2024-07-01 11:16:18 +0800

    Update variables.tf

--- samples/2405220030004811/src/variables.tf
+++ samples/2405220030004811/src/variables.tf
@@ -19,11 +19,3 @@ data "azurerm_client_config" "current" {
 data "external" "me" {
   program = ["az", "account", "show", "--query", "user"]
 }
-
-locals {
-  prefix = "abd-pl"
-  tags = {
-    Environment = "Demo"
-    Owner       = lookup(data.external.me.result, "name")
-  }
-}
