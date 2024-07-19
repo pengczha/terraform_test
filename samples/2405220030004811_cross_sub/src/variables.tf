628d24996a5d2a355523c384f224cbeea3acb528
pengczha <116861160+pengczha@users.noreply.github.com>
2024-07-19 10:28:38 +0800

    Create variables.tf

---
+++ samples/2405220030004811_cross_sub/src/variables.tf
@@ -0,0 +1,27 @@
+variable "cidr" {
+  type = string
+  default = "10.1.0.0/16"
+}
+
+variable "rg_name" {
+  type = string
+  default = "adb-pl-test-rg"
+}
+
+variable "hub_rg_name" {
+  type = string
+  default = "hub-test-rg"
+}
+
+variable "location" {
+  type = string
+  default = "westeurope"
+}
+
+variable "workspace_prefix" {
+  type = string
+  default = "abd-pl"
+}
+
+data "azurerm_client_config" "current" {
+}
