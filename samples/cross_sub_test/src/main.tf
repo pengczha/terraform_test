51f00a87ef905bcaee6118cb9a40c091ae366457
pengczha <116861160+pengczha@users.noreply.github.com>
2024-07-22 10:55:18 +0800

    Update main.tf

--- samples/cross_sub_test/src/main.tf
+++ samples/cross_sub_test/src/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   alias = "hub-sub"
   subscription_id = "792791c0-485d-4465-9c4b-e2c10fa9c810"
   tenant_id = "16b3c013-d300-468d-ac64-7eda0820b6d3"
-  clinet_secret = ${{ secrets.TERRAFORMSPPENGCZHA_SECRET }}
+  clinet_secret = $(SECRET)
   client_id = "db9af9ca-7fc8-4050-ab31-c3bbd6f28899"
   features {}
 }
@@ -23,7 +23,7 @@ provider "azurerm" {
   alias = "spoke-sub"
   subscription_id = "5f0f314b-4646-4212-ac9f-1b1de1860bcd"
   tenant_id = "16b3c013-d300-468d-ac64-7eda0820b6d3"
-  clinet_secret = ${{ secrets.TERRAFORMSPPENGCZHA_SECRET }}
+  clinet_secret =  $(SECRET)
   client_id = "db9af9ca-7fc8-4050-ab31-c3bbd6f28899"
   features {}
 }
