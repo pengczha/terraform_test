42b7d7ac56640514a7237ac6c37a4d69dbb6e2d9
pengczha <116861160+pengczha@users.noreply.github.com>
2025-04-30 15:16:24 +0800

    Update main.tf

--- samples/2504280030002135/src/main.tf
+++ samples/2504280030002135/src/main.tf
@@ -1,1 +1,16 @@
 
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
